7e051c8c491e70006417ee48a3b1ce723451a76a
omer evron <omerevron011@gmail.com>
2022-11-30 23:33:53 +0200

    fixed landing image size

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -8,9 +8,9 @@ function Landing() {
   const {user, UserData }=UserAuth()
   return (
     <div className='landing font-sans'>
-    <Card className={'container max-w-[600px] bg-indigo-300'}>
+    <Card className={'container max-w-[600px] flex flex-col items-center bg-indigo-300'}>
       <h3 className='text-center p-10'>Hello <span className='font-bold text-emerald-900 capitalize'>{user&&UserData.displayName}</span>,<br/> Welcome <br/> to Your Garden Journal</h3>
-      <img  src="/garden-of-life-logo-vector.svg" alt="" />
+      <img className='max-w-[400px]' src="/garden-of-life-logo-vector.svg" alt="" />
    {!user&& <Link className={'link-reset'} to={'/login'}>
       <div className='mt-4 btn btn_log_in'>Click here to Log in</div>
     </Link>}
